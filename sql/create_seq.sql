aa9418878704e83ce2eb5fad2709ab35f229dddb
HailFederer <urimaumdero@naver.com>
2017-09-19 14:36:19 +0900

    first commit

---
+++ sql/create_seq.sql
@@ -0,0 +1,17 @@
+-- seq_category
+
+drop SEQUENCE seq_category;
+
+create SEQUENCE seq_category
+start with 1
+increment by 1
+maxvalue 9999999999;
+
+
+
+drop SEQUENCE seq_post;
+
+create SEQUENCE seq_post
+start with 1
+increment by 1
+maxvalue 9999999999;
